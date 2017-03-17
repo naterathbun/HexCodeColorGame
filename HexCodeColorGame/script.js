a8b215723632ba591734c5b7207a3a105e5552b6
Nathan Rathbun <naterathbun@gmail.com>
2017-03-16 22:40:00 -0400

    Finished base functionality including scoring

--- HexCodeColorGame/script.js
+++ HexCodeColorGame/script.js
@@ -1,11 +1,34 @@
-/// <reference path="Scripts/jquery-3.1.1.intellisense.js" />
-
+/// <reference path="~/Scripts/jquery-3.1.1.js" />
 window.onload = function () {
+    loadNewColor();
+}
 
-    alert("If you can see this, the JS file loaded.");
-    // not getting any jquery functionality here.... why not?
-};
+$(document).ready(function () {
+    
+    $("#mysteryBox").on("click", function (event) {
+        loadNewColor();
+    });
 
+    $(".guessBox").on("click", function (event) {
+
+        if (convertRGBToHex($(event.target).css("background-color")) == $("#mysteryBox p").text()) {
+            var oldCount = $("#numberCorrectBox p:nth-child(2)").text();
+            $("#numberCorrectBox p:nth-child(2)").text(oldCount * 1 + 1);
+            alert("Correct!");
+            loadNewColor();
+        }
+        else {
+            var oldCount = $("#numberWrongBox p:nth-child(2)").text();
+            $("#numberWrongBox p:nth-child(2)").text(oldCount * 1 + 1);
+        }
+    });
+});
+
+function loadNewColor() {
+    var newColor = getRandomColor();
+    $("#mysteryBox p").text(newColor);
+    assignColorsToGuessBoxes(newColor);
+}
 
 function getRandomColor() {
     var hexDigits = '0123456789ABCDEF';
@@ -14,4 +37,25 @@ function getRandomColor() {
         colorCode += hexDigits[Math.floor(Math.random() * 16)];
     }
     return colorCode;
+}
+
+function assignColorsToGuessBoxes(newColor) {
+    var correctBox = Math.floor(Math.random() * 4) + 1;
+
+    for (var i = 1; i < 5; i++) {
+        if (i == correctBox) {
+            $("#guessBox" + correctBox).css("background-color", newColor);
+        }
+        else {
+            $("#guessBox" + i).css("background-color", getRandomColor());
+        }
+    }
+}
+
+function convertRGBToHex(rgb) {
+    rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+    function hex(x) {
+        return ("0" + parseInt(x).toString(16)).slice(-2);
+    }
+    return ("#" + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3])).toUpperCase();
 }
